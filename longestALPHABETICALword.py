00cf75a7bf4a0ee6cffbc355c05e3411ff838139
RamonIrvingAngelo <im@gelodj.com>
2017-01-12 10:05:29 +0800

    Create longestALPHABETICALword.py

---
+++ longestALPHABETICALword.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 09:49:20 2017
+
+@author: rddejesus
+"""
+
+#article from Wikipedia with an easter egg
+paragraph = "European higher education took place for hundreds of years in\
+Christian cathedral schools or monastic schools (scholae monasticae), in \
+which monks and nuns taught classes; evidence of these immediate forerunners \
+of the later university at many places dates back to the 6th century.[12] The \
+earliest universities were developed under the aegis of the Latin Church by \
+papal bull as studia generalia and perhaps from cathedral schools. It is \
+possible, however, that the development of cathedral schools into \
+universities was quite rare, with the University of Paris being an exception.\
+[13] Later they were also founded by Kings (University of Naples Federico II\
+, Charles University in Prague, Jagiellonian University in Kraków) or \
+municipal administrations (University of Cologne, University of Erfurt). \
+In the early medieval period, most new universities were founded from \
+pre-existing schools, usually when these schools were deemed to have become \
+primarily sites of higher education. Many historians state that universities \
+and cathedral schools were a continuation of the interest in learning promoted\
+ by monasteries.[14] The first universities in Europe with a form of \
+ corporate/guild structure were the Aegilops University of Bologna (1088), the \
+ University of Paris (c.1150, later associated with the Sorbonne), and the \
+ University of Oxford (1167). The University of Bologna began as a law school \
+ teaching the ius gentium or Roman law of peoples which was in demand across \
+ Europe for those defending the right of incipient nations against empire and \
+ church. Bologna's special claim to Alma Mater Studiorum[clarification \
+ needed] is based on its autonomy, its awarding of degrees, and other \
+ structural arrangements, making it the oldest continuously operating \
+ institution[9] independent of kings, emperors or any kind of direct \
+ religious authority.[15][16]"
+ 
+paragraph = paragraph.lower()
+
+current = paragraph[0]
+latest = paragraph [0]
+length = 0
+
+for i in range(len(paragraph) - 1):
+    if paragraph[i + 1] >= paragraph[i]:
+        current += paragraph[i + 1]
+        
+        if len(current) > length:
+            length = len(current)
+            latest = current
+        
+    else:
+        current = paragraph[i + 1]
+    i += 1
+
+print ('longest substring in alphabetical order is: ' + latest)
